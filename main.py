48abf2a1137cc76d0d8a443d4ec7ba9f03bc672c
Nadi Duno <nadiduno@gmail.com>
2023-07-26 16:11:11 -0300

    Refactoring Code

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-df = pd.read_csv("https://raw.githubusercontent.com/nadiduno/dataClient/main/cancelamentos.csv")
-df=df.drop("CustomerID",axis=1)
-#dropna para deletar valores vazios
+import plotly.graph_objs as go
+import plotly.offline as pyo
+
+st.set_page_config(page_title='Contas/Clientes', layout='wide')
+
+df = pd.read_csv('https://raw.githubusercontent.com/nadiduno/dataClient/main/cancelamentos.csv')
+
+df=df.drop({'CustomerID','idade','sexo'},axis=1)
 df = df.dropna()
 
 st.title('Dados de Cancelamentos dos cliente')
 st.dataframe(df)
-
-st.title('Gráfico')
-st.plotly_chart(px.histogram(df),use_container_width=True)
-
-# fig =px.line(df)
-# st.dataframe(df.style.background_gradient(cmap='Blues'))
+st.title('Gráficos')
+# fig = px.histogram(df, x="duracao_contrato",color="cancelou")
+# st.plotly_chart(fig)
+for coluna in df.columns:
+    fig = px.histogram(df, x=coluna,color="cancelou")
+    st.plotly_chart(fig)
 
 
 st.write('Porcentagem de cancelamentos sem análisis')
@@ -31,6 +36,14 @@ df = df[df["duracao_contrato"] != "Monthly"]
 df2 = df2[df2["ligacoes_callcenter"]<5]
 #EliminandoDias de atraso de 20 
 df2 = df2[df2["dias_atraso"]<=20]
-st.write('Reduzindo cancelamentos, seguindo os análises')
+st.write('Seguindo os análises')
 st.dataframe(df2["Cancelou 0/Não 1/Sim"].value_counts(normalize=True).map("{:.2%}".format))
 
+st.title('Reduziremos cancelamentos de 56.71% a 18.40%')
+st.caption('Seguindo as recomendações:')
+st.caption('- Tirar a forma de pagamento mensal o colocar um valor menor.')
+st.caption('- Evitar que o cliente chegue a 20 dias de atraso no pagamento, porque cancela.')
+st.caption('- Evitar que o cliente ligue al call center mais de 5 (resolver seu problema antes)')
+
+st.write('Dados disponíveis em https://github.com/nadiduno/dataClient')
+st.markdown('Um aplicativo feito com <💜 /> [Nadi Duno](https://www.linkedin.com/in/nadiduno/) © Julho 2023')
